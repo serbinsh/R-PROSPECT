b15fd1e22d28b660600c1a389b59f799315ab6ef
Shawn P. Serbin <serbin@wisc.edu>
2013-05-07 10:05:31 -0500

    Modifications to package directory structure.  Updates to examples.R script.

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -1,50 +1,78 @@
+####################################################################################################
+# R-PROSPECT Examples
+#
+# Last Updated: 05/07/2013 by SPS
+####################################################################################################
+
+
 #--------------------------------------------------------------------------------------------------#
-# Examples
+# Close all devices and delete all variables.
+rm(list=ls(all=TRUE))   # clear workspace
+graphics.off()          # close any open graphics
+closeAllConnections()   # close any open connections to files
 #--------------------------------------------------------------------------------------------------#
 
 
+#--------------------------------------------------------------------------------------------------#
+### Load required library
 library(Rprospect)
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Output directory.  Needs to be set
+output.dir <- '/Users/serbin/Data/'
+#--------------------------------------------------------------------------------------------------#
+
+
 #################### Some examples for testing #####################
 
 ## Run PROSPECT-4
 LRT <- prospect4(2,65,0.004,0.002)
 plot(LRT[,1],LRT[,2],type="l",xlab="Wavelength (nm)",
-     ylab="Reflectance/Transmittance (%)")
+     ylab="Reflectance/Transmittance (%)",main="PROSPECT - 4")
 
 ## Run PROSPECT-5
 LRT <- prospect5(2,65,30,0.006,0.003)
 plot(LRT[,1],LRT[,2],type="l",xlab="Wavelength (nm)",
-     ylab="Reflectance/Transmittance (%)")
+     ylab="Reflectance/Transmittance (%)",main="PROSPECT - 5")
 
 ## Run PROSPECT-5B
 LRT <- prospect5B(2,65,30,0.05,0.006,0.003)
 plot(LRT[,1],LRT[,2],type="l",xlab="Wavelength (nm)",
-     ylab="Reflectance/Transmittance (%)")
+     ylab="Reflectance/Transmittance (%)",main="PROSPECT - 5B")
 
 
 ## Example PROSPECT Inversion
 
 ## Load test data
-data(poplar)  # test poplar dataset
-data(clover)  # test clover dataset
-data(beech)   # test beech dataset
+poplar <- read.table(file=paste(.path.package("Rprospect"),"/extdata/poplar.txt",sep=""),
+                     header=T)   # test poplar dataset
+clover <- read.table(file=paste(.path.package("Rprospect"),"/extdata/clover.txt",sep=""),
+                     header=T)   # test clover dataset
+beech <- read.table(file=paste(.path.package("Rprospect"),"/extdata/beech.txt",sep=""),
+                     header=T)   # test beech dataset
 
+# Choose spec sample to invert
 spectra = clover  # chosen spectra to invert
+
+# Plot R & T Spectra
 plot(spectra[,1],spectra[,2],type="l",lwd=2.5,ylim=c(0,1))
 lines(spectra[,1],1-spectra[,3],lwd=2.5,col="dark grey")
 box(lwd=2.2)
 
-## Test PROSPECT inversion
+## ---- Test PROSPECT inversion ----
 waves <- spectra[,1]
 refl <- spectra[,2]
 tran <- spectra[,3]
 
 ## PROSPECT-4
 inv = invprospect(refl,tran,model=4,method="DEoptim",strategy=2,threshold=0.01,cpus=4)
-plot.prospect.inv(inv,outdir='/Users/serbin/Data/Dropbox/FERST_Code/Test_Rprospect/',file='test_prospect.inv4')
+plot.prospect.inv(inv,outdir=output.dir,file='test_prospect.inv4')
 
 ## PROSPECT-5
 inv = invprospect(refl,tran,model=5,"DEoptim",strategy=2,threshold=0.01,cpus=4)
-plot.prospect.inv(inv,outdir='/Users/serbin/Data/Dropbox/FERST_Code/Test_Rprospect/',file='test_prospect.inv5')
+plot.prospect.inv(inv,outdir=output.dir,file='test_prospect.inv5')
+#--------------------------------------------------------------------------------------------------#
 
-
+### end of file
